0af374be861d606986cf96322e8edbe0a3509c40
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-19 12:27:07 +0100

    feat(kernel): add project directory retrieval method
    - Added a new method `getProjectDir` to the Kernel class to return
      the project directory path.
    - Updated file header with copyright and project information.

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,11 +1,35 @@
 <?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 19/02/2025, 12:27
+ *
+ * @project IDMarinas Template Symfony
+ * @see     https://github.com/idmarinas/template-symfony
+ *
+ * @file    Kernel.php
+ * @date    18/02/2025
+ * @time    22:49
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
 
 namespace App;
 
+use Override;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
-    use MicroKernelTrait;
+	use MicroKernelTrait;
+
+	#[Override]
+	public function getProjectDir (): string
+	{
+		return dirname(__DIR__);
+	}
 }
