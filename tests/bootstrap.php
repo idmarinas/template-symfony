27665a74ba36137e071a90111430e060b7af062f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-19 19:50:50 +0100

    fix(tests): update bootstrap.php for improved environment handling
    - Refactored the bootstrap process to enhance readability.
    - Added use statements for Kernel and Filesystem.
    - Improved spacing and formatting for consistency.
    - Added logic to remove cache and log directories if they exist.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,41 @@
 <?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 19/02/2025, 19:26
+ *
+ * @project IDMarinas Template Symfony
+ * @see     https://github.com/idmarinas/template-symfony
+ *
+ * @file    bootstrap.php
+ * @date    19/02/2025
+ * @time    19:50
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
 
+use App\Kernel;
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Filesystem\Filesystem;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
-if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
-    require dirname(__DIR__).'/config/bootstrap.php';
+if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
+	require dirname(__DIR__) . '/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+	new Dotenv()->bootEnv(dirname(__DIR__) . '/.env');
+}
+
+$kernel = new Kernel('test', true);
+$filesystem = new Filesystem();
+
+if ($filesystem->exists($kernel->getCacheDir())) {
+	$filesystem->remove($kernel->getCacheDir());
+}
+
+if ($filesystem->exists($kernel->getLogDir())) {
+	$filesystem->remove($kernel->getLogDir());
 }
