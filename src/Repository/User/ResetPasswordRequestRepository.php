c5e40a8b4aaf6afa6290335de2f612a7fff84cab
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-20 16:10:36 +0100

    feat(user): add UserRepository and ResetPasswordRequestRepository classes
    - Introduced UserRepository for managing User entities.
    - Introduced ResetPasswordRequestRepository for managing
      ResetPasswordRequest entities.
    - Both repositories extend their respective abstract classes
      and utilize Doctrine's ManagerRegistry for database operations.

---
+++ src/Repository/User/ResetPasswordRequestRepository.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 20/02/2025, 15:51
+ *
+ * @project IDMarinas Template Symfony
+ * @see     https://github.com/idmarinas/template-symfony
+ *
+ * @file    ResetPasswordRequestRepository.php
+ * @date    20/02/2025
+ * @time    15:51
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace App\Repository\User;
+
+use App\Entity\User\ResetPasswordRequest;
+use Doctrine\Persistence\ManagerRegistry;
+use Idm\Bundle\User\Model\Repository\AbstractResetPasswordRequestRepository;
+
+final class ResetPasswordRequestRepository extends AbstractResetPasswordRequestRepository
+{
+	public function __construct (ManagerRegistry $registry)
+	{
+		parent::__construct($registry, ResetPasswordRequest::class);
+	}
+}
