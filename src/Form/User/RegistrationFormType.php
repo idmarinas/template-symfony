03edaac8c44a6ad0c9c31e89e1c485969aecba9c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-20 16:10:18 +0100

    feat(form): add RegistrationFormType for user registration functionality
    - Introduced a new form type for user registration in the application.
    - The form type extends the AbstractRegistrationFormType for enhanced
      functionality.

---
+++ src/Form/User/RegistrationFormType.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 20/02/2025, 15:51
+ *
+ * @project IDMarinas Template Symfony
+ * @see     https://github.com/idmarinas/template-symfony
+ *
+ * @file    RegistrationFormType.php
+ * @date    20/02/2025
+ * @time    15:51
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace App\Form\User;
+
+use Idm\Bundle\User\Model\Form\AbstractRegistrationFormType;
+
+class RegistrationFormType extends AbstractRegistrationFormType {}
