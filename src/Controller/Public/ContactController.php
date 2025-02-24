9a2ee02f2f17834cea5aec105890d5fbcf16f145
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-24 17:55:11 +0100

    feat(contact): add web route to ContactController for handling requests

--- src/Controller/Public/ContactController.php
+++ src/Controller/Public/ContactController.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 24/02/2025, 17:46
+ * Last modified by "IDMarinas" on 24/02/2025, 17:54
  *
  * @project IDMarinas Template Symfony
  * @see     https://github.com/idmarinas/template-symfony
@@ -20,13 +20,22 @@
 namespace App\Controller\Public;
 
 use App\Form\Contact\ContactFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Idm\Bundle\Common\Model\Controller\AbstractContactController;
 use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/contact', name: 'contact_')]
 class ContactController extends AbstractContactController
 {
+	#[Route(path: '/web', name: 'web', methods: ['GET', 'POST'])]
+	public function index (Request $request, EntityManagerInterface $entityManager): Response
+	{
+		return parent::index($request, $entityManager);
+	}
+
 	protected function getContactForm (): FormInterface
 	{
 		return $this->createForm(ContactFormType::class);
