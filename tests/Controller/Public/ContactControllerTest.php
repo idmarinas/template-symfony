be169f041c753c4b4e95bf9f045e96645964cdb0
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-24 18:51:57 +0100

    test: add ContactControllerTest with various submission scenarios
    - Implement tests for the ContactController that cover:
      - Successful GET request to the contact page.
      - Successful form submission with valid data.
      - Successful form submission without an email.
      - Unsuccessful form submission with invalid data.
    
    This enhances the test coverage for the contact functionality.

---
+++ tests/Controller/Public/ContactControllerTest.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 24/02/2025, 18:13
+ *
+ * @project IDMarinas Template Symfony
+ * @see     https://github.com/idmarinas/template-symfony
+ *
+ * @file    ContactControllerTest.php
+ * @date    24/02/2025
+ * @time    18:51
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace App\Tests\Controller\Public;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Zenstruck\Foundry\Test\Factories;
+use function Zenstruck\Foundry\faker;
+
+class ContactControllerTest extends WebTestCase
+{
+	use Factories;
+
+	public function testIndex ()
+	{
+		$client = static::createClient();
+		$client->request(Request::METHOD_GET, '/public/contact/web');
+
+		$this->assertResponseIsSuccessful();
+
+		$this->assertPageTitleContains('IDMarinas Common Bundle');
+		$this->assertSelectorTextContains('h2', 'Your information');
+	}
+
+	public function testSubmit ()
+	{
+		$client = static::createClient();
+		$client->request(Request::METHOD_POST, '/public/contact/web');
+
+		$client->submitForm('contact_form_buttonSubmit', [
+			'contact_form[name]'     => faker()->firstName(),
+			'contact_form[lastName]' => faker()->lastName(),
+			'contact_form[email]'    => faker()->email(),
+			'contact_form[comment]'  => faker()->text(),
+			'contact_form[consent]'  => true,
+		]);
+
+		$this->assertResponseIsSuccessful();
+
+		$this->assertSelectorTextContains('.flash-success', 'Your comments/questions have been sent successfully.');
+	}
+
+	public function testSubmitNotEmail ()
+	{
+		$client = static::createClient();
+		$client->request(Request::METHOD_POST, '/public/contact/web');
+
+		$client->submitForm('contact_form_buttonSubmit', [
+			'contact_form[name]'     => faker()->firstName(),
+			'contact_form[lastName]' => faker()->lastName(),
+			'contact_form[comment]'  => faker()->text(),
+			'contact_form[consent]'  => true,
+		]);
+
+		$this->assertResponseIsSuccessful();
+
+		$this->assertSelectorTextContains('.flash-success', 'Your comments/questions have been sent successfully.');
+	}
+
+	public function testSubmitInvalid ()
+	{
+		$client = static::createClient();
+		$client->request(Request::METHOD_POST, '/public/contact/web');
+
+		$client->submitForm('contact_form_buttonSubmit', [
+			'contact_form[name]'     => faker()->name(),
+			'contact_form[lastName]' => faker()->lastName(),
+			'contact_form[comment]'  => faker()->text(400),
+			'contact_form[consent]'  => false,
+		]);
+
+		$this->assertResponseIsUnprocessable();
+
+		$this->assertSelectorTextContains('form', 'This value should be true.');
+	}
+}
