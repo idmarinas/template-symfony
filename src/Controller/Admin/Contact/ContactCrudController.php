89ea437bf383198fdc282da3ce0397179b3e790e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-28 11:38:32 +0100

    feat(controller): enhance ContactCrudController with soft delete support
    - Added CrudActionRestoreTrait to enable soft delete functionality.
    - Injected EntityManagerInterface for managing entity state.
    - Updated configureFields method to include 'isDeleted' field as a switch.
    - Overrode configureActions method to handle soft delete filtering.

--- src/Controller/Admin/Contact/ContactCrudController.php
+++ src/Controller/Admin/Contact/ContactCrudController.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 20/02/2025, 18:13
+ * Last modified by "IDMarinas" on 28/02/2025, 11:37
  *
  * @project IDMarinas Template Symfony
  * @see     https://github.com/idmarinas/template-symfony
@@ -20,12 +20,44 @@
 namespace App\Controller\Admin\Contact;
 
 use App\Entity\Contact\Contact;
+use App\Traits\Admin\Crud\CrudActionRestoreTrait;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use Idm\Bundle\Common\Model\Controller\Admin\AbstractContactCrudController;
+use Override;
+use function Symfony\Component\Translation\t;
 
 class ContactCrudController extends AbstractContactCrudController
 {
+	use CrudActionRestoreTrait;
+
+	public function __construct (private readonly EntityManagerInterface $entityManager) {}
+
 	public static function getEntityFqcn (): string
 	{
 		return Contact::class;
+	}
+
+	public function configureFields (string $pageName): iterable
+	{
+		$t = fn($message) => t($message, [], 'IdmUserBundle');
+		yield from parent::configureFields($pageName);
+
+		yield BooleanField::new('isDeleted', $t('crud.common.is_deleted'))
+			->hideOnForm()
+			->renderAsSwitch(false)
+			->setVirtual(true)
+		;
+	}
+
+	#[Override]
+	public function configureActions (Actions $actions): Actions
+	{
+		if ($this->entityManager->getFilters()->isEnabled('softdeleteable')) {
+			$this->entityManager->getFilters()->disable('softdeleteable');
+		}
+
+		return parent::configureActions($this->restoreAction($actions));
 	}
 }
