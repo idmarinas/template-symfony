45c9c5ed71490c9672971e19dfaa7f972f97c8bb
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-28 11:38:24 +0100

    feat(controller): enhance UserCrudController with restore action
    - Added CrudActionRestoreTrait for soft delete functionality.
    - Injected EntityManagerInterface for database operations.
    - Overrode configureActions method to integrate restore action.

--- src/Controller/Admin/User/UserCrudController.php
+++ src/Controller/Admin/User/UserCrudController.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 20/02/2025, 15:51
+ * Last modified by "IDMarinas" on 28/02/2025, 11:37
  *
  * @project IDMarinas Template Symfony
  * @see     https://github.com/idmarinas/template-symfony
@@ -20,12 +20,30 @@
 namespace App\Controller\Admin\User;
 
 use App\Entity\User\User;
+use App\Traits\Admin\Crud\CrudActionRestoreTrait;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use Idm\Bundle\User\Model\Controller\Admin\AbstractUserCrudController;
+use Override;
 
 final class UserCrudController extends AbstractUserCrudController
 {
+	use CrudActionRestoreTrait;
+
+	public function __construct (private readonly EntityManagerInterface $entityManager) {}
+
 	public static function getEntityFqcn (): string
 	{
 		return User::class;
+	}
+
+	#[Override]
+	public function configureActions (Actions $actions): Actions
+	{
+		if ($this->entityManager->getFilters()->isEnabled('softdeleteable')) {
+			$this->entityManager->getFilters()->disable('softdeleteable');
+		}
+
+		return parent::configureActions($this->restoreAction($actions));
 	}
 }
