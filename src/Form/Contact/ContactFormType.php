c9bc5e551d7b6da70c03e41ba4a445bc659aaeca
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-02 20:09:24 +0100

    refactor(form): change namespace for ContactFormType
    - Updated the namespace of ContactFormType from
      App\Form\Contact to Shared\Form\Contact.
    - Adjusted the use statement for the Contact entity to reflect the
      new namespace.

--- src/Form/Contact/ContactFormType.php
+++ src/Form/Contact/ContactFormType.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 20/02/2025, 18:09
+ * Last modified by "IDMarinas" on 02/03/2025, 18:39
  *
  * @project IDMarinas Template Symfony
  * @see     https://github.com/idmarinas/template-symfony
@@ -17,10 +17,10 @@
  * @since   1.0.0
  */
 
-namespace App\Form\Contact;
+namespace Shared\Form\Contact;
 
-use App\Entity\Contact\Contact;
 use Idm\Bundle\Common\Model\Form\AbstractContactFormType;
+use Shared\Entity\Contact\Contact;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ContactFormType extends AbstractContactFormType
