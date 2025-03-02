88db290032d4fd19e43df600d3d1c32c8b96005c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-02 20:13:18 +0100

    fix(index): update Kernel usage and add APP_ID parameter
    Updated the index.php file to change the usage of the Kernel class from
    App\Kernel to Shared\Kernel. Additionally, modified the return function
    to include the APP_ID parameter when creating a new Kernel instance.
    
    This change improves the application structure and enhances the
    configuration flexibility.

--- public/index.php
+++ public/index.php
@@ -1,9 +1,26 @@
 <?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 02/03/2025, 18:21
+ *
+ * @project IDMarinas Template Symfony
+ * @see     https://github.com/idmarinas/template-symfony
+ *
+ * @file    index.php
+ * @date    02/03/2025
+ * @time    20:13
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
 
-use App\Kernel;
+use Shared\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+	return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG'], $context['APP_ID']);
 };
