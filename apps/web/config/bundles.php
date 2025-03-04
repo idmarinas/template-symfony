fc0dd3f6e2f8cb6c840d724cfa3fc3836223ae7b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-04 14:52:02 +0100

    feat(bundles): add initial bundles configuration file
    - Added a new file `bundles.php` to the `apps/web/config` directory.
    - This file includes the initial configuration for bundles in the Symfony
      project.

---
+++ apps/web/config/bundles.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 02/03/2025, 18:30
+ *
+ * @project IDMarinas Template Symfony
+ * @see     https://github.com/idmarinas/template-symfony
+ *
+ * @file    bundles.php
+ * @date    02/03/2025
+ * @time    18:11
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+return [];
