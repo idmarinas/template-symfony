f99909795ef6c80c97730f343b26850966846c7d
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-13 22:56:56 +0100

    feat(api): add initial configuration files for API services
    - Added services.yaml for service parameters and defaults.
    - Created bundles.php to return an empty array for bundles.
    - Added routes.yaml for API routes configuration.
    - Included .gitkeep files in tests and src directories to maintain
      structure.

---
+++ apps/api/config/bundles.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 13/03/2025, 22:50
+ *
+ * @project IDMarinas Template Symfony
+ * @see     https://github.com/idmarinas/template-symfony
+ *
+ * @file    bundles.php
+ * @date    13/03/2025
+ * @time    22:56
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+return [];
