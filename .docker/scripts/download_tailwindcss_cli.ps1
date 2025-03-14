d1efe27d2a7e5089ca6e6282895fed218330b9c9
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-14 22:32:23 +0100

    feat(scripts): enhance TailwindCSS download script with version input
    - Added parameters for version and Windows version selection.
    - Improved user prompts for better interaction.
    - Implemented error handling for directory creation and file downloads.
    - Updated download URLs to dynamically reflect the chosen version.

--- .docker/scripts/download_tailwindcss_cli.ps1
+++ .docker/scripts/download_tailwindcss_cli.ps1
@@ -1,18 +1,106 @@
-# wget https://github.com/tailwindlabs/tailwindcss/releases/download/v4.0.11/tailwindcss-linux-x64-musl -P /var/www/html/var/tailwind/cli
-# chmod +x /var/www/html/var/tailwind/cli/tailwindcss-linux-x64-musl
+param(
+  [String]$Version,
+  [Boolean]$Windows
+)
 
-#Invoke-WebRequest -Uri "https://github.com/tailwindlabs/tailwindcss/releases/download/v4.0.11/tailwindcss-linux-x64-musl" -OutFile $folderPath
+[String]$VersionDefault = "4.0.14"
+[Boolean]$WindowsDefault = $false
 
-$url = "https://github.com/tailwindlabs/tailwindcss/releases/download/v4.0.11/tailwindcss-linux-x64-musl"
-$folderPath = "F:\Projects\template-symfony\var\tailwind\cli"
-$fileName = "tailwindcss-linux-x64-musl"
-$fullPath = Join-Path -Path $folderPath -ChildPath $fileName
-
-# Crear la carpeta si no existe
-if (-not (Test-Path -Path $folderPath))
+if (-not $Version)
 {
-  New-Item -ItemType Directory -Path $folderPath
+  $Version = Read-Host "Please enter the version of TailwindCSS to download (Default $VersionDefault)"
+
+  if (-not $Version)
+  {
+    $Version = $VersionDefault
+  }
+}
+
+if (-not $Windows)
+{
+  $WindowsInput = Read-Host "Do you want the Windows version too? (Default $WindowsDefault)"
+
+  if (-not $WindowsInput)
+  {
+    $Windows = $WindowsDefault
+  }
+  else
+  {
+    $yes = @("y", "ye", "yes")
+    $no = @("n", "no", "not")
+    if ($yes -contains $WindowsInput)
+    {
+      $Windows = $true
+    }
+    elseif ($no -contains $WindowsInput)
+    {
+      $Windows = $false
+    }
+    else
+    {
+      try
+      {
+        $Windows = [bool]::Parse($WindowsInput)
+      }
+      catch
+      {
+        Write-Host "The input '$WindowsInput' is invalid. Please enter 'y', 'yes', 'n', 'no', 'true' or 'false'." -ForegroundColor Red
+
+        exit
+      }
+    }
+  }
+}
+
+[String]$FolderPath = "F:\Projects\template-symfony\var\tailwind\cli"
+[String]$LinuxFile = "tailwindcss-linux-x64-musl"
+[String]$WindowsFile = "tailwindcss-windows-x64.exe"
+[String]$Url = "https://github.com/tailwindlabs/tailwindcss/releases/download"
+[String]$LinuxUrl = "$Url/v$Version/$LinuxFile".Trim()
+[String]$WindowsUrl = "$Url/v$Version/$WindowsFile".Trim()
+[String]$LinuxPath = Join-Path -Path $FolderPath -ChildPath $LinuxFile
+[String]$WindowsPath = Join-Path -Path $FolderPath -ChildPath $WindowsFile
+
+Write-Host "Downloading v$Version version of TailwindCSS" -ForegroundColor Blue
+
+# Create the folder if it does not exist
+if (-not (Test-Path -Path $FolderPath))
+{
+  try
+  {
+    New-Item -ItemType Directory -Path $FolderPath
+    Write-Host "Directory created at $FolderPath" -ForegroundColor Green
+  }
+  catch
+  {
+    Write-Host "Failed to create directory at $FolderPath. Please check permissions." -ForegroundColor Red
+    exit
+  }
 }
 
 # Download the file using BitsTransfer
-Start-BitsTransfer -Source $url -Destination $fullPath
+try
+{
+  Start-BitsTransfer -Source $LinuxUrl -Destination $LinuxPath
+  Write-Host "Linux version downloaded successfully to $LinuxPath".Trim() -ForegroundColor Green
+}
+catch
+{
+  Write-Host "Failed to download the Linux version from $LinuxUrl" -ForegroundColor Red
+  exit
+}
+
+# Download the file using BitsTransfer
+if ($Windows)
+{
+  try
+  {
+    Start-BitsTransfer -Source $WindowsUrl -Destination $WindowsPath
+    Write-Host "Windows version downloaded successfully to $WindowsPath" -ForegroundColor Green
+  }
+  catch
+  {
+    Write-Host "Failed to download the Windows version from $WindowsUrl" -ForegroundColor Red
+    exit
+  }
+}
