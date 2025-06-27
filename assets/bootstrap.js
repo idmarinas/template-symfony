aeb1f1345437229a1e1f303449659fce76c45609
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-06-27 14:16:39 +0200

    chore(ui): register idm-ui-bundle in stimulus app

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,26 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "idmarinas" on 27/06/2025, 14:15
+ *
+ * @project IDMarinas Template Symfony
+ * @see https://github.com/idmarinas/template-symfony
+ *
+ * @file bootstrap.js
+ * @date 27/06/2025
+ * @time 14:16
+ *
+ * @author Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since 1.0.0
+ */
 
-const app = startStimulusApp();
+import {startStimulusApp} from '@symfony/stimulus-bundle'
+import {registerIdmUiBundle} from '@idmarinas/ui-bundle'
+
+const app = startStimulusApp()
+registerIdmUiBundle(app)
+
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
