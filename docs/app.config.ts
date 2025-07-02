58c4b306f0ca837d5ed68ff11d181e7bfefec81f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-07-02 19:13:07 +0200

    docs(tpl): initial docs template

---
+++ docs/app.config.ts
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 02/07/2025, 20:39
+ *
+ * @project IDMarinas Template Symfony
+ * @see https://github.com/idmarinas/template-symfony
+ *
+ * @file app.config.ts
+ * @date 02/07/2025
+ * @time 19:28
+ *
+ * @author Iván Diaz Marinas (IDMarinas)
+ * @license undefined
+ *
+ * @since 1.0.0
+ */
+
+export default defineAppConfig({
+	header: {
+		title: 'IDMarinas Template Symfony',
+		logo: {
+			light: '/icon96.webp',
+			dark: '/icon96.webp',
+			alt: 'IDMarinas Template Symfony',
+		}
+	},
+	socials: {
+		x: 'https://x.com/idmarinas',
+		github: 'https://github.com/idmarinas',
+		discord: 'https://discord.gg/FXEZqpF',
+
+		facebook: 'https://facebook.com/idmarinas',
+		instagram: 'https://instagram.com/idmarinas',
+		linkedin: 'https://linkedin.com/idmarinas',
+		youtube: 'https://youtube.com/idmarinas',
+		twitch: 'https://twitch.com/idmarinas',
+		telegram: 'https://telegram.com/idmarinas',
+	},
+	github: {
+		url: 'https://github.com/idmarinas/template-symfony',
+		branch: '1.x',
+		rootDir: 'docs'
+	}
+})
