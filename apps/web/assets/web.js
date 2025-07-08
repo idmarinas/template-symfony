8bc72232adb81b350bb827892b3e7a403028ee87
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-07-08 17:59:51 +0200

    assets: import CSS into Web.js file

---
+++ apps/web/assets/web.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 08/07/2025, 16:27
+ *
+ * @project IDMarinas Template Symfony
+ * @see https://github.com/idmarinas/template-symfony
+ *
+ * @file web.js
+ * @date 08/07/2025
+ * @time 15:32
+ *
+ * @author Iván Diaz Marinas (IDMarinas)
+ * @license undefined
+ *
+ * @since 1.0.0
+ */
+
+import './styles/web.css'
