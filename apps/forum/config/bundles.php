63d9816db27c21c8db941b79d0de9aa514578a0e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-07-09 16:04:05 +0200

    config(bundles): update bundles enabled and disabled in Forum App

---
+++ apps/forum/config/bundles.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 19/06/2025, 18:57
+ *
+ * @project IDMarinas Template Symfony
+ * @see     https://github.com/idmarinas/template-symfony
+ *
+ * @file    bundles.php
+ * @date    02/03/2025
+ * @time    18:11
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license undefined
+ *
+ * @since   1.0.0
+ */
+
+return [
+	// Enabled necessary Bundles
+	TalesFromADev\Twig\Extra\Tailwind\Bridge\Symfony\Bundle\TalesFromADevTwigExtraTailwindBundle::class => ['all' => true],
+	// Disabled unnecessary Bundles
+];
