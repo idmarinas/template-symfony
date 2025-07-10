ec34f20fcbdc0f122080dba725fbb33ef169080f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-07-10 20:10:45 +0200

    refactor: moved ContactController.php to Feedback namespace

--- apps/web/src/Controller/Feedback/ContactController.php
+++ apps/web/src/Controller/Feedback/ContactController.php
@@ -2,14 +2,14 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 03/03/2025, 20:23
+ * Last modified by "IDMarinas" on 10/07/2025, 20:10
  *
  * @project IDMarinas Template Symfony
  * @see     https://github.com/idmarinas/template-symfony
  *
  * @file    ContactController.php
- * @date    20/02/2025
- * @time    18:08
+ * @date    10/07/2025
+ * @time    20:01
  *
  * @author  Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
@@ -17,20 +17,22 @@
  * @since   1.0.0
  */
 
-namespace Web\Controller\Public;
+namespace Web\Controller\Feedback;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Idm\Bundle\Common\Model\Controller\AbstractContactController;
+use Override;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Web\Form\Contact\ContactFormType;
+use Web\Form\Feedback\ContactFormType;
 
 #[Route('/contact', name: 'contact_')]
 class ContactController extends AbstractContactController
 {
-	#[Route(path: '/web', name: 'web', methods: ['GET', 'POST'])]
+	#[Route(path: '/', name: 'index', methods: ['GET', 'POST'])]
+	#[Override]
 	public function index (Request $request, EntityManagerInterface $entityManager): Response
 	{
 		return parent::index($request, $entityManager);
