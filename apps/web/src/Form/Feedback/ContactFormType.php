d5b98ffe460e2214ce66fad675f01c83384e6b0f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-07-10 20:11:50 +0200

    refactor: move from Core App to Web App

--- apps/web/src/Form/Feedback/ContactFormType.php
+++ apps/web/src/Form/Feedback/ContactFormType.php
@@ -2,14 +2,14 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 03/03/2025, 20:23
+ * Last modified by "IDMarinas" on 10/07/2025, 20:10
  *
  * @project IDMarinas Template Symfony
  * @see     https://github.com/idmarinas/template-symfony
  *
  * @file    ContactFormType.php
- * @date    20/02/2025
- * @time    18:08
+ * @date    10/07/2025
+ * @time    19:56
  *
  * @author  Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
@@ -17,10 +17,10 @@
  * @since   1.0.0
  */
 
-namespace Web\Form\Contact;
+namespace Web\Form\Feedback;
 
+use Core\Entity\Feedback\Contact;
 use Idm\Bundle\Common\Model\Form\AbstractContactFormType;
-use Shared\Entity\Contact\Contact;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ContactFormType extends AbstractContactFormType
--- apps/web/src/Form/User/RegistrationFormType.php
+++ apps/web/src/Form/User/RegistrationFormType.php
@@ -2,14 +2,14 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 03/03/2025, 20:23
+ * Last modified by "IDMarinas" on 10/07/2025, 20:10
  *
  * @project IDMarinas Template Symfony
  * @see     https://github.com/idmarinas/template-symfony
  *
  * @file    RegistrationFormType.php
- * @date    20/02/2025
- * @time    15:51
+ * @date    10/07/2025
+ * @time    19:55
  *
  * @author  Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
