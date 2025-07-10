b65d90d47f561842c876817d7f0d3a2727b5d17b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-07-10 16:30:57 +0200

    docs(nuxt): added configuration for llms

---
+++ docs/nuxt.config.ts
@@ -0,0 +1,23 @@
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 10/07/2025, 16:28
+ *
+ * @project IDMarinas Template Symfony
+ * @see https://github.com/idmarinas/template-symfony
+ *
+ * @file nuxt.config.ts
+ * @date 10/07/2025
+ * @time 16:30
+ *
+ * @author Iván Diaz Marinas (IDMarinas)
+ * @license undefined
+ *
+ * @since 1.0.0
+ */
+
+export default defineNuxtConfig({
+	llms: {
+		domain: 'https://your-site.com', // Change me
+	},
+})
