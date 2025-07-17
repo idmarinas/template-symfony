3905d098b7fe8a681fe920550666914edb756e6d
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-07-17 19:34:35 +0200

    chore(config): add bundles configuration for blog app

---
+++ apps/blog/config/bundles.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 17/07/2025, 19:28
+ *
+ * @project IDMarinas Template Symfony
+ * @see     https://github.com/idmarinas/template-symfony
+ *
+ * @file    bundles.php
+ * @date    02/03/2025
+ * @time    18:11
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+return [
+	// Enabled necessary Bundles
+	// Disabled unnecessary Bundles
+];
