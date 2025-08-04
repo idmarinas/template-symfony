4dd31b1055719f95fb47c87c22d66e02e48e4f3f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-04 11:50:30 +0200

    chore(docs): add <AppHeaderLogo /> component to display header logos

---
+++ docs/app/components/AppHeaderLogo.vue
@@ -0,0 +1,34 @@
+<!---
+  - Copyright 2025 (C) IDMarinas - All Rights Reserved
+  -
+  - Last modified by "IDMarinas" on 04/08/2025, 11:50
+  -
+  - @project IDMarinas Template Symfony
+  - @see https://github.com/idmarinas/template-symfony
+  -
+  - @file AppHeaderLogo.vue
+  - @date 04/08/2025
+  - @time 11:50
+  -
+  - @author Iván Diaz Marinas (IDMarinas)
+  - @license BSD 3-Clause License
+  -
+  - @since 1.0.0
+  -->
+
+<script setup lang="ts">
+const appConfig = useAppConfig()
+</script>
+
+<template>
+  <UColorModeImage
+    v-if="appConfig.header?.logo?.dark || appConfig.header?.logo?.light"
+    :light="appConfig.header?.logo?.light || appConfig.header?.logo?.dark"
+    :dark="appConfig.header?.logo?.dark || appConfig.header?.logo?.light"
+    :alt="appConfig.header?.logo?.alt || appConfig.header?.title"
+    class="h-16 w-auto shrink-0"
+  />
+  <span v-else>
+    {{ appConfig.header?.title || '{appConfig.header.title}' }}
+  </span>
+</template>
