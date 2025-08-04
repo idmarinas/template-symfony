7d1ad29397c5b340c036f0e17e68d994fb99c256
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-04 11:48:29 +0200

    chore(docs): add shared GitHub configuration file for reuse

---
+++ docs/shared.config.ts
@@ -0,0 +1,23 @@
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 04/08/2025, 11:29
+ *
+ * @project IDMarinas Template Symfony
+ * @see https://github.com/idmarinas/template-symfony
+ *
+ * @file shared.config.ts
+ * @date 04/08/2025
+ * @time 11:29
+ *
+ * @author Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since 1.0.0
+ */
+
+export const github = {
+	url: 'https://github.com/idmarinas/template-symfony',
+	branch: '1.x',
+	rootDir: 'docs'
+}
