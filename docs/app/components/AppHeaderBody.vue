6de5f6a079aeb6535dad91114688628c3a89a002
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-04 11:52:41 +0200

    chore(docs): implement <AppHeaderBody /> with navigation and version selector

---
+++ docs/app/components/AppHeaderBody.vue
@@ -0,0 +1,35 @@
+<!---
+  - Copyright 2025 (C) IDMarinas - All Rights Reserved
+  -
+  - Last modified by "IDMarinas" on 03/08/2025, 21:27
+  -
+  - @project IDMarinas Template Symfony
+  - @see https://github.com/idmarinas/template-symfony
+  -
+  - @file AppHeaderBody.vue
+  - @date 04/08/2025
+  - @time 11:52
+  -
+  - @author Iván Diaz Marinas (IDMarinas)
+  - @license BSD 3-Clause License
+  -
+  - @since 1.0.0
+  -->
+
+<script setup lang="ts">
+import type { ContentNavigationItem } from '@nuxt/content'
+
+const navigation = inject<Ref<ContentNavigationItem[]>>('navigation')
+</script>
+
+<template>
+	<DocVersionSelect />
+
+	<USeparator type="dashed" class="my-6" />
+
+  <UContentNavigation
+    highlight
+    variant="link"
+    :navigation="navigation"
+  />
+</template>
