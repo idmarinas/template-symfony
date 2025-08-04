10e487c6ba32f22586e6d7afba3af868a65b9e3d
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-04 11:49:10 +0200

    chore(docs): add <AppHeaderBody /> component

---
+++ docs/app/layouts/docs.vue
@@ -0,0 +1,33 @@
+<!---
+  - Copyright 2025 (C) IDMarinas - All Rights Reserved
+  -
+  - Last modified by "IDMarinas" on 01/08/2025, 19:04
+  -
+  - @project IDMarinas Template Symfony
+  - @see https://github.com/idmarinas/template-symfony
+  -
+  - @file docs.vue
+  - @date 04/08/2025
+  - @time 11:49
+  -
+  - @author Iván Diaz Marinas (IDMarinas)
+  - @license BSD 3-Clause License
+  -
+  - @since 1.0.0
+  -->
+
+<template>
+  <UContainer>
+    <UPage>
+      <template #left>
+        <UPageAside>
+          <DocsAsideLeftTop />
+
+          <AppHeaderBody />
+        </UPageAside>
+      </template>
+
+      <slot />
+    </UPage>
+  </UContainer>
+</template>
