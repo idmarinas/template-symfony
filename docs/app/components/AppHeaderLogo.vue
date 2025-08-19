c69182d0fc8a4cca5234029b851cb456ed24800a
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-19 17:31:22 +0200

    chore(docs): adjust logo size in <AppHeaderLogo /> component

--- docs/app/components/AppHeaderLogo.vue
+++ docs/app/components/AppHeaderLogo.vue
@@ -1,14 +1,14 @@
 <!---
   - Copyright 2025 (C) IDMarinas - All Rights Reserved
   -
-  - Last modified by "IDMarinas" on 04/08/2025, 11:50
+  - Last modified by "IDMarinas" on 19/08/2025, 17:19
   -
   - @project IDMarinas Template Symfony
   - @see https://github.com/idmarinas/template-symfony
   -
   - @file AppHeaderLogo.vue
-  - @date 04/08/2025
-  - @time 11:50
+  - @date 19/08/2025
+  - @time 17:31
   -
   - @author Iván Diaz Marinas (IDMarinas)
   - @license BSD 3-Clause License
@@ -26,7 +26,7 @@ const appConfig = useAppConfig()
     :light="appConfig.header?.logo?.light || appConfig.header?.logo?.dark"
     :dark="appConfig.header?.logo?.dark || appConfig.header?.logo?.light"
     :alt="appConfig.header?.logo?.alt || appConfig.header?.title"
-    class="h-16 w-auto shrink-0"
+    class="h-12 w-auto shrink-0"
   />
   <span v-else>
     {{ appConfig.header?.title || '{appConfig.header.title}' }}
