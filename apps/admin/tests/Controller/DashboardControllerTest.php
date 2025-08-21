19456045fed34d3439c2623f503019de927080ee
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-21 19:12:42 +0200

    chore(tests): add DashboardControllerTest and LoginControllerTest for admin section validation

---
+++ apps/admin/tests/Controller/DashboardControllerTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 21/08/2025, 19:09
+ *
+ * @project IDMarinas Template Symfony
+ * @see     https://github.com/idmarinas/template-symfony
+ *
+ * @file    DashboardControllerTest.php
+ * @date    21/08/2025
+ * @time    13:16
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+declare(strict_types=1);
+
+namespace Admin\Tests\Controller;
+
+use Core\Tests\CreateClientTrait;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class DashboardControllerTest extends WebTestCase
+{
+	use CreateClientTrait;
+
+	public function testDashboard (): void
+	{
+		$client = static::createClient();
+		$client->request(Request::METHOD_GET, '/admin');
+
+		$this->assertResponseStatusCodeSame(401);
+	}
+}
---
+++ apps/admin/tests/Controller/LoginControllerTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 21/08/2025, 19:09
+ *
+ * @project IDMarinas Template Symfony
+ * @see     https://github.com/idmarinas/template-symfony
+ *
+ * @file    LoginControllerTest.php
+ * @date    23/02/2025
+ * @time    19:38
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Admin\Tests\Controller;
+
+use Core\Tests\CreateClientTrait;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class LoginControllerTest extends WebTestCase
+{
+	use CreateClientTrait;
+
+	public function testLoginUser (): void
+	{
+		$client = static::createClient();
+		$client->request(Request::METHOD_GET, '/admin/login');
+
+		$this->assertResponseIsSuccessful();
+
+		$client->submitForm('Sign in', [
+			'_username' => 'admin@admin.admin',
+			'_password' => 'admin.pass.1234.uj',
+		]);
+
+		$this->assertResponseIsSuccessful();
+	}
+}
