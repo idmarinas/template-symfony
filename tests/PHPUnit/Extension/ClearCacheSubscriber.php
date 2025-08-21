c4eb063c948b4ed40ead4055c3df1068c99c21e8
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-21 18:51:01 +0200

    chore(tests): add ClearCacheSubscriber for cache and logs cleanup after PHPUnit test suites

---
+++ tests/PHPUnit/Extension/ClearCacheSubscriber.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 21/08/2025, 18:49
+ *
+ * @project IDMarinas Template Symfony
+ * @see     https://github.com/idmarinas/template-symfony
+ *
+ * @file    ClearCacheSubscriber.php
+ * @date    21/08/2025
+ * @time    17:33
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Core\Tests\PHPUnit\Extension;
+
+use Core\Kernel;
+use Override;
+use PHPUnit\Event\TestSuite\Finished;
+use PHPUnit\Event\TestSuite\FinishedSubscriber;
+use Symfony\Component\Filesystem\Filesystem;
+
+class ClearCacheSubscriber implements FinishedSubscriber
+{
+	#[Override]
+	public function notify (Finished $event): void
+	{
+		$name = $event->testSuite()->name();
+		if (!$event->testSuite()->isWithName() || str_contains($name, 'phpunit.xml')) {
+			return;
+		}
+
+		$kernel = new Kernel('test', true, $name);
+		$fs = new Filesystem();
+
+		if ($fs->exists($kernel->getCacheDir())) {
+			$fs->remove($kernel->getCacheDir());
+		}
+
+		if ($fs->exists($kernel->getLogDir())) {
+			$fs->remove($kernel->getLogDir());
+		}
+	}
+}
