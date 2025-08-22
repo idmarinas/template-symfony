bd884c78fc986a0e87bca7907093cbfb7281fd5e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-22 16:21:57 +0200

    chore(tests): add HomeControllerTest for blog, forum, and core apps

---
+++ tests/HomeControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 21/08/2025, 19:09
+ *
+ * @project IDMarinas Template Symfony
+ * @see     https://github.com/idmarinas/template-symfony
+ *
+ * @file    HomeControllerTest.php
+ * @date    23/02/2025
+ * @time    19:38
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Core\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class HomeControllerTest extends WebTestCase
+{
+	use CreateClientTrait;
+
+	public function testHomePage (): void
+	{
+		$client = static::createClient();
+		$client->request(Request::METHOD_GET, '/');
+
+		$this->assertResponseStatusCodeSame(404);
+	}
+}
