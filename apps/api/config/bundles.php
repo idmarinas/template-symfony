c33835ba1d4dfc00f2df60ec714b3c4544a91940
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-25 20:05:02 +0200

    chore(config): enable Twig bundles for dev and test environments

--- apps/api/config/bundles.php
+++ apps/api/config/bundles.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 17/07/2025, 18:51
+ * Last modified by "IDMarinas" on 25/08/2025, 20:03
  *
  * @project IDMarinas Template Symfony
  * @see     https://github.com/idmarinas/template-symfony
@@ -18,12 +18,12 @@
  */
 
 return [
-	// Active only for Dev
-	Symfony\Bundle\TwigBundle\TwigBundle::class                      => ['dev' => true],
-	Twig\Extra\TwigExtraBundle\TwigExtraBundle::class                => ['dev' => true],
+	// Active only for Dev/Test
+	Symfony\Bundle\TwigBundle\TwigBundle::class                      => ['dev' => true, 'test' => true],
+	Twig\Extra\TwigExtraBundle\TwigExtraBundle::class                => ['dev' => true, 'test' => true],
+	Symfony\UX\TwigComponent\TwigComponentBundle::class              => ['dev' => true, 'test' => true],
 	// Disable unnecessary Bundles
 	Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => false],
 	Symfony\UX\StimulusBundle\StimulusBundle::class                  => ['all' => false],
 	Symfony\UX\Turbo\TurboBundle::class                              => ['all' => false],
-	Symfony\UX\TwigComponent\TwigComponentBundle::class              => ['all' => false],
 ];
