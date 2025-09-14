cf08b405bbc93a5c3efc5f087ca593452ba90ba4
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-09-14 12:26:17 +0200

    chore(docker): add PowerShell script to build DEV Docker image and prune unused images

---
+++ .docker/scripts/BuildDockerImageDEV.ps1
@@ -0,0 +1,23 @@
+function Get-Env
+{
+  param([string]$VarName, [string]$File = "$PSScriptRoot/../../.env")
+
+  $line = Get-Content $File | Where-Object { $_ -match "^$VarName\s*=" } | Select-Object -First 1
+
+  if ($line)
+  {
+    $value = ($line -split "=", 2)[1].Trim()
+    return $value -replace '^["\x27]|["\x27]$', ''
+  }
+  return $null
+}
+
+$appVersion = Get-Env -VarName "APP_VERSION"
+$appTitle = Get-Env -VarName "APP_TITLE"
+
+Write-Host "DEV Building Docker Imagen" -BackgroundColor Red
+Write-Host "$appTitle" -BackgroundColor Green
+Write-Host "Tag: $appVersion" -BackgroundColor Blue
+
+docker build --target dev -f .docker/Dockerfile -t "idmarinas/template_symfony:$appVersion-dev" .
+docker image prune -f
