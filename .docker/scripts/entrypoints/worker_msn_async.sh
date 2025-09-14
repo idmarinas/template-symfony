b8d8b6004909fb10c288ee33457b087606e1389f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-09-14 12:27:17 +0200

    chore(docker): add entrypoint scripts for messenger workers (scheduler and async)

---
+++ .docker/scripts/entrypoints/worker_msn_async.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+##
+# Copyright 2025 (C) IDMarinas - All Rights Reserved
+#
+# Last modified by "IDMarinas" on 14/09/2025, 12:15
+#
+# @project IDMarinas Template Symfony
+# @see https://github.com/idmarinas/template-symfony
+#
+# @file worker_msn_async.sh
+# @date 11/09/2025
+# @time 10:22
+#
+# @author Iván Diaz Marinas (IDMarinas)
+# @license BSD 3-Clause License
+#
+# @since 1.0.0
+#
+
+docker-php-entrypoint
+php bin/console messenger:consume async --time-limit=3600 --memory-limit=128M --limit=100
