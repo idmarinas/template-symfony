e7c440fff9e9d0d3b58ae034e574e6dca6120598
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-09-24 13:29:13 +0200

    chore(deployer): fix variable reference for app version in upload task

--- .deployer/task/upload_files.php
+++ .deployer/task/upload_files.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 14/09/2025, 11:32
+ * Last modified by "IDMarinas" on 24/09/2025, 12:22
  *
  * @project IDMarinas Template Symfony
  * @see     https://github.com/idmarinas/template-symfony
@@ -23,18 +23,18 @@ import('recipe/common.php');
 
 desc('Upload files to server');
 task('deploy:upload_files', function () {
-    writeln('<fg=blue>Subiendo archivos a {{text_prod}}...</>');
-    upload('./', '{{release_path}}', [
-        'flags'   => '-azPh',
-        'options' => [
-            '--include=compose.yaml',
-            '--include=compose.prod.yaml',
-            '--exclude=**/*',
-            '--chmod=F440',
-        ],
-    ]);
-    writeln('<fg=blue>Subiendo idmarinas_pfc_{{app_version}}.tar a {{text_prod}}...</>');
-    upload('./.deployer/idmarinas_pfc_{{app_version}}.tar', '{{release_path}}', [
-        'options' => ['--chmod=F750'],
-    ]);
+	writeln('<fg=blue>Subiendo archivos a {{text_prod}}...</>');
+	upload('./', '{{release_path}}', [
+		'flags'   => '-azPh',
+		'options' => [
+			'--include=compose.yaml',
+			'--include=compose.prod.yaml',
+			'--exclude=**/*',
+			'--chmod=F440',
+		],
+	]);
+	writeln('<fg=blue>Subiendo idmarinas_pfc_{{app/version}}.tar a {{text_prod}}...</>');
+	upload('./.deployer/idmarinas_pfc_{{app/version}}.tar', '{{release_path}}', [
+		'options' => ['--chmod=F750'],
+	]);
 });
