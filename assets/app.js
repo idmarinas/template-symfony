fcc573f1bbaf6619b3bea6f4a2bac4bb495043ce
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-10-19 18:36:41 +0200

    chore: update app.css import path
    - Change import path from `./styles/app.css` to `./styles/app/app.css` in `assets/app.js`

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 19/10/2025, 18:35
+ *
+ * @project IDMarinas Template Symfony
+ * @see https://github.com/idmarinas/template-symfony
+ *
+ * @file app.js
+ * @date 19/10/2025
+ * @time 18:36
+ *
+ * @author Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since 1.0.0
+ */
+
 import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
@@ -5,6 +23,6 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/app/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
