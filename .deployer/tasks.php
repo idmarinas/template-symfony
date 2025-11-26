b7b78db7cc264ccf846e499497f36c09dfda69d3
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-11-26 13:01:53 +0100

    chore: update deployment task paths
    - Renamed `task/` directory to `tasks/` for consistency.
    - Updated all `import()` statements in `.deployer/tasks.php` to reflect the new directory structure.

--- .deployer/tasks.php
+++ .deployer/tasks.php
@@ -4,13 +4,13 @@ namespace Deployer;
 
 import('recipe/common.php');
 
-import(__DIR__ . '/task/docker.php');
-import(__DIR__ . '/task/upload_files.php');
-import(__DIR__ . '/task/doctrine.php');
-import(__DIR__ . '/task/maintenance.php');
-import(__DIR__ . '/task/symfony_workers.php');
-import(__DIR__ . '/task/download_files.php');
-import(__DIR__ . '/task/restore_volumes.php');
+import(__DIR__ . '/tasks/docker.php');
+import(__DIR__ . '/tasks/upload_files.php');
+import(__DIR__ . '/tasks/doctrine.php');
+import(__DIR__ . '/tasks/maintenance.php');
+import(__DIR__ . '/tasks/symfony_workers.php');
+import(__DIR__ . '/tasks/download_files.php');
+import(__DIR__ . '/tasks/restore_volumes.php');
 
 task('deploy:prepare', [
 	'deploy:info',
