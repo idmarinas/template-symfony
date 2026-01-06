96b2535c5102650b5257da8d5a2693dbaffbdcd7
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-06 13:59:52 +0100

    chore(docker): remove redundant metadata comments in entrypoint scripts
    - Cleaned up copyright and metadata comments from `worker_scheduler.sh` and `worker_async.sh`.
    - Retained only essential logic for Docker entrypoints.

--- .docker/scripts/entrypoints/worker_async.sh
+++ .docker/scripts/entrypoints/worker_async.sh
@@ -1,24 +1,4 @@
 #!/bin/sh
 
-
-
-##
-# Copyright 2025 (C) IDMarinas - All Rights Reserved
-#
-# Last modified by "IDMarinas" on 17/10/2025, 18:37
-#
-# @project IDMarinas Template Symfony
-# @see https://github.com/idmarinas/template-symfony
-#
-# @file worker_async.sh
-# @date 17/10/2025
-# @time 18:41
-#
-# @author Iván Diaz Marinas (IDMarinas)
-# @license BSD 3-Clause License
-#
-# @since 1.0.0
-#
-
 docker-php-entrypoint
 php bin/console messenger:consume async --time-limit=3600 --memory-limit=128M --limit=100
--- .docker/scripts/entrypoints/worker_scheduler.sh
+++ .docker/scripts/entrypoints/worker_scheduler.sh
@@ -1,25 +1,5 @@
 #!/bin/sh
 
-
-
-##
-# Copyright 2025 (C) IDMarinas - All Rights Reserved
-#
-# Last modified by "IDMarinas" on 17/10/2025, 18:37
-#
-# @project IDMarinas Template Symfony
-# @see https://github.com/idmarinas/template-symfony
-#
-# @file worker_scheduler.sh
-# @date 17/10/2025
-# @time 18:41
-#
-# @author Iván Diaz Marinas (IDMarinas)
-# @license BSD 3-Clause License
-#
-# @since 1.0.0
-#
-
 # Scheduler parece que no es compatible con la opción --limit
 docker-php-entrypoint
 php bin/console messenger:consume scheduler_default --time-limit=3600 --memory-limit=128M
